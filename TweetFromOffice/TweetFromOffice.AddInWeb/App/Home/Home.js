e3813bfbdb45c0484f1834d89f8b33056ecc6ea2
Martin Kearn <mkearn@microsoft.com>
2015-08-03 16:36:26 +0100

    Base UI for Ofice add-in

--- TweetFromOffice/TweetFromOffice.AddInWeb/App/Home/Home.js
+++ TweetFromOffice/TweetFromOffice.AddInWeb/App/Home/Home.js
@@ -3,25 +3,25 @@
 (function () {
     "use strict";
 
+    // These are my personal Twitter keys so you should store your own keys here, you can create an application at http://apps.twitter.com. However this is just a sample so if you cannot be bothered to create your own, use these and expect that the may change without notice
+    var twitterConsumerKey = "arWUup59V9t3oA7JVtoaP42Ws";
+    var twitterConsumerSecret = "hMrWNIqDLy6Ee3cOLMvt2gVZpYOT1wWv7ptO4tfEg23Li3tq1H";
+    var twitterAccessToken = "";
+    var twitterAccessTokenSecret = "";
+
     // The initialize function must be run each time a new page is loaded
     Office.initialize = function (reason) {
         $(document).ready(function () {
             app.initialize();
 
-            $('#get-data-from-selection').click(getDataFromSelection);
+            $('#SendTweet').click(SendTweet);
+
         });
     };
 
-    // Reads data from current document selection and displays a notification
-    function getDataFromSelection() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    app.showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    app.showNotification('Error:', result.error.message);
-                }
-            }
-        );
+    function SendTweet() {
+        var tweet = $("#Tweet")[0].value;
+        app.showNotification('You will tweet', tweet);
     }
+
 })();
